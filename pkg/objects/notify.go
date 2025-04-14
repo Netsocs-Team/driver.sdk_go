ac9c0b9e199313db70aac521e3710b87400f6d07
Angel Duarte <cuervodelviento@gmail.com>
2025-04-14 17:39:07 -0400

    feat: follow the notify object shape

--- pkg/objects/notify.go
+++ pkg/objects/notify.go
@@ -6,36 +6,40 @@ import (
 	"github.com/goccy/go-json"
 )
 
-const NOTIFIER_STATE_UNKNOWN = "notifier.state.unknown"
-const NOTIFIER_STATE_IDLE = "notifier.state.idle"
-const NOTIFIER_STATE_BUSY = "notifier.state.busy"
-const NOTIFIER_STATE_ERROR = "notifier.state.error"
-const NOTIFIER_ACTION_NOTIFY = "notifier.action.notify"
+const NOTIFY_STATE_UNKNOWN = "notify.state.unknown"
+const NOTIFY_STATE_IDLE = "notify.state.idle"
+const NOTIFY_STATE_BUSY = "notify.state.busy"
+const NOTIFY_STATE_ERROR = "notify.state.error"
+const NOTIFY_ACTION_CREATE = "notify.action.create"
 
-type NotifierObject interface {
+type NotifyObject interface {
 	RegistrableObject
 }
 
-type NotificationPayload struct {
-	Title    string `json:"title,omitempty"`
-	Content  string `json:"content,omitempty"`
-	ImageURL string `json:"image_url,omitempty"`
-	AudioURL string `json:"audio_url,omitempty"`
-	VideoURL string `json:"video_url,omitempty"`
+type CreatePayload struct {
+	Message        string `json:"message"`
+	Title          string `json:"title,omitempty"`
+	NotificationID string `json:"notification_id,omitempty"`
+	Target         string `json:"target,omitempty"`
+	Data           struct {
+		ImageURLs []string `json:"image_urls,omitempty"`
+		AudioURLs []string `json:"audio_urls,omitempty"`
+		VideoURLs []string `json:"video_urls,omitempty"`
+	} `json:"data,omitempty"`
 }
 
 type notifierObject struct {
 	controller ObjectController
 	metadata   ObjectMetadata
-	setupFn    func(notifierObject NotifierObject, oc ObjectController) error
-	notifyFn   func(notifierObject NotifierObject, oc ObjectController, payload NotificationPayload) error
+	setupFn    func(notifierObject NotifyObject, oc ObjectController) error
+	createFn   func(notifierObject NotifyObject, oc ObjectController, payload CreatePayload) error
 }
 
 // GetAvailableActions implements NotifierObject.
 func (n *notifierObject) GetAvailableActions() []ObjectAction {
 	return []ObjectAction{
 		{
-			Action: NOTIFIER_ACTION_NOTIFY,
+			Action: NOTIFY_ACTION_CREATE,
 			Domain: n.GetMetadata().Domain,
 		},
 	}
@@ -44,10 +48,10 @@ func (n *notifierObject) GetAvailableActions() []ObjectAction {
 // GetAvailableStates implements NotifierObject.
 func (n *notifierObject) GetAvailableStates() []string {
 	return []string{
-		NOTIFIER_STATE_UNKNOWN,
-		NOTIFIER_STATE_IDLE,
-		NOTIFIER_STATE_BUSY,
-		NOTIFIER_STATE_ERROR,
+		NOTIFY_STATE_UNKNOWN,
+		NOTIFY_STATE_IDLE,
+		NOTIFY_STATE_BUSY,
+		NOTIFY_STATE_ERROR,
 	}
 }
 
@@ -59,14 +63,14 @@ func (n *notifierObject) GetMetadata() ObjectMetadata {
 
 // RunAction implements NotifierObject.
 func (n *notifierObject) RunAction(id string, action string, payload []byte) (map[string]string, error) {
-	var p NotificationPayload
+	var p CreatePayload
 	if err := json.Unmarshal(payload, &p); err != nil {
 		return nil, err
 	}
 
 	switch action {
-	case NOTIFIER_ACTION_NOTIFY:
-		if err := n.notifyFn(n, n.controller, p); err != nil {
+	case NOTIFY_ACTION_CREATE:
+		if err := n.createFn(n, n.controller, p); err != nil {
 			return nil, err
 		}
 		return nil, nil
@@ -95,16 +99,16 @@ func (n *notifierObject) UpdateStateAttributes(attributes map[string]string) err
 
 type NewNotifierObjectProps struct {
 	Metadata ObjectMetadata
-	SetupFn  func(notifierObject NotifierObject, oc ObjectController) error
-	NotifyFn func(notifierObject NotifierObject, oc ObjectController, payload NotificationPayload) error
+	SetupFn  func(notifierObject NotifyObject, oc ObjectController) error
+	CreateFn func(notifierObject NotifyObject, oc ObjectController, payload CreatePayload) error
 }
 
-func NewNotifierObject(props NewNotifierObjectProps) NotifierObject {
+func NewNotifierObject(props NewNotifierObjectProps) NotifyObject {
 	n := &notifierObject{
 
 		metadata: props.Metadata,
 		setupFn:  props.SetupFn,
-		notifyFn: props.NotifyFn,
+		createFn: props.CreateFn,
 	}
 
 	return n
