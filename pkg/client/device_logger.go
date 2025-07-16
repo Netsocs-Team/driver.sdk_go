53776b047024b9cab608bb1d141a2f0dbe6a54ea
Leonardo Galindo <leonardo.galindo@netsocs.com>
2025-07-15 20:38:39 -0400

    feat: add WriteLog function to device_logger for logging device actions

---
+++ pkg/client/device_logger.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func (d *NetsocsDriverClient) WriteLog(deviceId int, log string, params ...string) (*http.Response, error) {
+	url := ""
+	if d.isSSL {
+		url = fmt.Sprintf("https://%s/devices/audit-logs", d.driverHubHost)
+	} else {
+		url = fmt.Sprintf("http://%s/devices/audit-logs", d.driverHubHost)
+	}
+	action := "NULL"
+	if len(params) > 0 {
+		action = params[0]
+	}
+
+	resp, err := resty.New().R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Authorization", fmt.Sprintf("Bearer %s", d.token)).
+		SetBody(map[string]interface{}{
+			"device_id": deviceId,
+			"message":   log,
+			"action":    action,
+		}).
+		Post(url)
+
+	if err != nil {
+		return nil, err
+	}
+	return resp.RawResponse, nil
+}
